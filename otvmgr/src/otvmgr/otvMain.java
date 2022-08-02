cdeb5843950c6dbe9a4b114a57a648c3959fc2fb
spark380@naver.com <spark380@naver.com>
2022-08-02 17:37:19 +0900

    V01

---
+++ otvmgr/src/otvmgr/otvMain.java
@@ -0,0 +1,31 @@
+package otvmgr;
+
+public class otvMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		int iIndex = 0;
+		
+		while(true)
+		{
+			iIndex++;
+			System.out.println("Hello otv " + iIndex);
+			
+			if(iIndex >= 10000)
+			{
+				iIndex = 0;
+			}
+			else
+			{
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch (Exception e) 
+				{
+					// TODO: handle exception
+				}
+			}
+		}
+	}
+}
